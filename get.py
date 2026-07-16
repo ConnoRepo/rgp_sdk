1c424c179c83c5cbbfb889875787f1fa2388b89b
connor hruby <connorjhruby@gmail.com>
2026-07-15 22:23:43 -0600

    simple parser for the calendar widget

--- get.py
+++ get.py
@@ -1,4 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import datetime
 
 url = "https://app.rockgympro.com/b/widget/"
 
@@ -10,8 +13,48 @@ params = {
     "end": "1788674400",
 }
 
-r = requests.get(url=url, params=params, timeout=10)
+def convert_response_to_json(raw_response):
 
-print(r.url)
-print(r.status_code)
-print(r.json())
+    json_response = raw_response.json()
+
+    return json_response
+
+def parse_event(event: dict):
+
+    title_soup = BeautifulSoup(event["title"], "html.parser").get_text()
+    start_soup = BeautifulSoup(event["start"], "html.parser").get_text()
+    end_soup   = BeautifulSoup(event["end"], "html.parser").get_text()
+
+    start_time = datetime.fromisoformat(start_soup)
+    end_time   = datetime.fromisoformat(end_soup)
+
+    cleaned_map = {
+        "title" : title_soup,
+        "start" : start_time,
+        "end"   : end_time,
+    }
+
+    return cleaned_map
+
+def convert_json_to_dataframe(json_response: dict):
+
+    event_list = []
+
+    for event in json_response: 
+
+        cleaned_event = parse_event(event=event)
+        event_list.append(cleaned_event)
+
+    df = pd.DataFrame(event_list)
+
+    return df
+
+if __name__ == "__main__":
+
+    response = requests.get(url=url, params=params, timeout=10)
+
+    json_response = convert_response_to_json(response)
+
+    result = convert_json_to_dataframe(json_response=json_response)
+
+    print(result)
