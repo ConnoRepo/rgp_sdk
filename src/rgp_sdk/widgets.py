c3831827b95589eed56021b4f4f708e3a060596d
connor hruby <connorjhruby@gmail.com>
2026-07-23 20:30:32 -0600

    adding in missing widget event

--- src/rgp_sdk/widgets.py
+++ src/rgp_sdk/widgets.py
@@ -15,7 +15,7 @@ class Widgets:
     def get_facilityJ_widget_guids(self, facilityCode, **filters : Unpack[ValidWidgetFilters]):
         return self._conn.get(path=f"widgets/events/facility/{facilityCode}", params=filters)\
 
-    def get_events_fromJ_guid(self, facilityCode, eventWidgetGuid, startDateTime, endDateTime):
+    def get_events_from_facility_guid(self, facilityCode, eventWidgetGuid, startDateTime, endDateTime):
 
         # need to validate that these are the names of the params that the api is looking for
         params = {
@@ -24,3 +24,10 @@ class Widgets:
         }
 
         return self._conn.get(path=f"widgets/events/facility/{facilityCode}/{eventWidgetGuid}", params=params)
+
+    def get_events_widget_guid(self, eventWidgetGuid, **filters : Unpack[ValidWidgetFilters]):
+
+        if filters not in ["startDateTime", "endDateTime"]:
+            raise ValueError(f"Invalid Parameter Provided: {filters}")
+
+        return self._conn.get(path=f"widgets/events/{eventWidgetGuid}", params=filters)
