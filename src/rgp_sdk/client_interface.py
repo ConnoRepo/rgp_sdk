ed5082e83ad4385638c9aca6f967c4b0c25ddfdb
connor hruby <connorjhruby@gmail.com>
2026-07-23 21:01:40 -0600

    added plurals to the interface.py

--- src/rgp_sdk/client_interface.py
+++ src/rgp_sdk/client_interface.py
@@ -1,32 +1,32 @@
 from ._http import Connection
-from .customers import Customer
+from .customers import Customers
 from .daily_numbers import DailyNumbers
 from .sales import Sales
 from .bookings import Bookings
-from .check_ins import CheckIn
+from .check_ins import CheckIns
 from .facilities import Facilities
 from .invoices import Invoices
 from .ping import Ping
 from .settings import Settings
 from .versions import Versions
 from .staff import Staff
-from .timeclocks import Timeclock
+from .timeclocks import Timeclocks
 from .widgets import Widgets
 
 class RGP:
 
     def __init__(self, username, api_key):
         self._conn = Connection(username, api_key)
-        self.customer = Customer(self._conn)
+        self.customer = Customers(self._conn)
         self.daily_numbers = DailyNumbers(self._conn)
         self.sales = Sales(self._conn)
         self.bookings = Bookings(self._conn)
-        self.check_ins = CheckIn(self._conn)
+        self.check_ins = CheckIns(self._conn)
         self.facilities = Facilities(self._conn)
         self.invoices = Invoices(self._conn)
         self.ping = Ping(self._conn)
         self.settings = Settings(self._conn)
         self.versions = Versions(self._conn)
         self.staff = Staff(self._conn)
-        self.timeclock = Timeclock(self._conn)
+        self.timeclock = Timeclocks(self._conn)
         self.widgets = Widgets(self._conn)
