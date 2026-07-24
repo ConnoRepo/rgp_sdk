462ac9399882b3855d61c0e01f831a8b94314deb
connor hruby <connorjhruby@gmail.com>
2026-07-23 18:45:24 -0600

    adding checkin get method

---
+++ src/rgp_sdk/check_ins.py
@@ -0,0 +1,26 @@
+from typing import TypedDict, Unpack
+
+class ValidCheckinTypes(TypedDict, total=False):
+    startId : int
+    customerGuid : str
+    customerDetails : bool
+    remoteOnly : bool
+    startDateTime : str
+    endDateTime : str
+    limit : int
+    page : int
+
+class CheckIn: 
+
+    def __init__(self, conn):
+        self._conn = conn
+
+    def get_facility_checkins(self, facilityCode, **filters : Unpack[ValidCheckinTypes]):
+        return self._conn.get(path=f"checkins/facility/{facilityCode}", params=filters)
+
+    def get_singular_checkin_details(self, facilityCode, checkinId):
+        return self._conn.get(path=f"checkins/facility/{facilityCode}/{checkinId}")
+
+    def get_active_checkins(self, facilityCode):
+        return self._conn.get(path=f"checkins/active/facility/{facilityCode}")
+        
